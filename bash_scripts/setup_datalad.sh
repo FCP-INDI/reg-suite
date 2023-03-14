006dd150bc2b6d203fa660f6675b225a46255f46
amygutierrez <amy.gutierrez@childmind.org>
2023-03-14 15:39:43 -0400

    reverting vision to run regression locally. Will try just running it through git actions

--- bash_scripts/setup_datalad.sh
+++ bash_scripts/setup_datalad.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-PROJECT="$1"
+VERSION="$1"
 
 git config --global user.email "CMI_CPAC_Support@childmind.org"
 git config --global user.name "Theodore (Machine User)"
@@ -8,8 +8,11 @@ wget -O- http://neuro.debian.net/lists/focal.us-tn.libre | sudo tee /etc/apt/sou
 sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
 sudo apt-get update
 sudo apt-get install datalad git-annex-standalone
-pip install datalad-osf
+pip3 install datalad-osf
 
-datalad clone ${PROJECT} reg-data
+datalad clone osf://qjn8d/ reg-data
 
-echo reg-data
+if [ ${VERSION} == 'lite' ]; then
+    find . -name reg-data/reg_5mm_pack -print
+elif [ ${VERSION} == 'full' ]; then
+    find . -name reg-data/raw -print
--- commands/run.py
+++ commands/run.py
@@ -4,6 +4,16 @@ import pandas as pd
 import numpy as np
 import click
 
+#def run_cpac(version, datapath=None, output=None, access_flag=False): 
+    #if version == 'lite' or 'local':
+        #preconfigs = 'default benchmark-FNIRT rbc-options'
+    #elif version == 'full':
+        #preconfigs = 'default rbc-options benchmark-FNIRT fmriprep-options ndmg fx-options abcd-options ccs-options rodent monkey'
+
+    #cmd = ['./bash_scripts/run_cpac.sh', datapath, output, preconfigs]
+    #result = subprocess.run(cmd, stdout=subprocess.PIPE)
+    #return
+
 @click.command()
 @click.option('--lite', is_flag=True, help='Run lite regression test. This will use 5mm datasets with ' 
               'shortened timeseries for faster processing. Runs on the following pipelines: default,'
@@ -12,41 +22,27 @@ import click
 @click.option('--full', is_flag=True, help='Run full regression test. This will run full dataset '
               '(17 subjects from 4 Sites) on 9 pipelines. Full version should be selected if ready '
               'to merge to main branch.')
-@click.option('--data', '-d', required=False, type=str, help='Path to input BIDS data')
-@click.option('--outdir', '-o', required=True, type=str, help='Path to output directory. working,'
-              'log, and output subdirectories will be stored in this path.')
 
-
-def run(lite, full, data=None, outdir=None):
+def run(lite, full):
     """
     Run C-PAC with either "lite" or "full" regression datasets
     """
+
     if lite:
         version = 'lite'
     elif full:
         version = 'full'
-    else:
-        version = 'local'
     click.echo(f"Selected {version} version of regression suite")
-    click.echo(f"data path {data}")
-    click.echo(f"output path {outdir}")
     
-    if (version == 'lite' or 'full') and data is not None:
-        datapath=data
-        click.echo(f'\n*******************************************************************\n'
-                f'You have selected {version} regression version, but added a data path.\n'
-                f'Will not run {version} datasets. Will instead run datasets in {data}\n'
-                f'*******************************************************************\n')
-    if (version == 'local') and data is None:
-        raise Exception(f'Must add data input path using --data flag')
-    if (version == 'lite' or 'full') and data is None:
-        if version == 'lite':
-            project = 'projectID'
-        elif version == 'full':
-            project = 'projectID'
-            
-        cmd = ['./bash_scripts/setup_datalad.sh', project]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE)
-        datapath = result.stdout.decode('utf-8')
+    cmd = ['bash', './bash_scripts/setup_datalad.sh', version]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE)
+    datapath = result.stdout.decode()
+    print(datapath)
+    #if lite: 
+        #datapath = os.path.join(datapath,"resampled_5mm_datasets")
+    #elif full: 
+        #datapath = os.path.join(datapath,'raw','cpac_regtest_data.tar.gz')
+    
+    #output = run_cpac(version, datapath, outdir, access_flag)
 
-    return(datapath, outdir)
+    return(datapath)
