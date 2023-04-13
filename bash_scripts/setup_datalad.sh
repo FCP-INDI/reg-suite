92e121b81e86dac990d31a8be26d276a6b59571e
amygutierrez <amygutierrezbme@gmail.com>
2023-04-13 15:31:57 -0400

    adding executable permissions to downloaded data

--- bash_scripts/setup_datalad.sh
+++ bash_scripts/setup_datalad.sh
@@ -3,17 +3,19 @@
 VERSION="$1"
 GITHUB_WORKSPACE="$2"
 
-git config --global user.email "amygutierrezbme@gmail.com"
-git config --global user.name "amygutierrez"
+git config --global user.email "CMI_CPAC_Support@childmind.org"
+git config --global user.name "Theodore (Machine User)"
 
 pip3 install osfclient 1> /dev/null
 osf -p qjn8d clone ${GITHUB_WORKSPACE}/reg-data 1> /dev/null
 
 if [ ${VERSION} == 'lite' ]; then
     datapath=$(find "$(pwd -P)" -name reg_5mm_pack -print)
+    chmod +x ${datapath}
     echo "datapath=${datapath}"
 
 elif [ ${VERSION} == 'full' ]; then
     datapath=$(find "$(pwd -P)" -name raw -print)
+    chmod +x ${datapath}
     echo "datapath=${datapath}"
 fi
