f52ad2d310fed694bf14356d43742d536113986c
Theodore (Machine User) <CMI_CPAC_Support@childmind.org>
2023-06-30 14:03:05 -0400

    added run name

--- commands/correlate.py
+++ commands/correlate.py
@@ -5,9 +5,9 @@ from commands.bin import parse_yaml
 
 
 @click.command()
-@click.option('--pipeline1', '-o', required=True, type=str, help='Path to output directory from CPAC run '
+@click.option('--pipeline1', required=True, type=str, help='Path to output directory from CPAC run '
               'to correlate against pipeline2')
-@click.option('--pipeline2', '-o', required=True, type=str, help='Path to output directory from CPAC run '
+@click.option('--pipeline2', required=True, type=str, help='Path to output directory from CPAC run '
               'to correlate against pipeline1')
 @click.option('--workspace','-w', type=str, help = 'Github workspace. This input is generated in git actions '
               'and should remain as $GITHUB_WORKSPACE')
@@ -19,8 +19,9 @@ def correlate(pipeline1, pipeline2, workspace):
     """
 
     git_home = os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + os.sep + os.pardir)
+    run_name = ((pipeline2.split(workspace+'lite'))[1]).replace('/','_')
 
-    parse_yaml.cpac_yaml(pipeline1, pipeline2, f'{workspace}/correlations', 'run', 1)
+    parse_yaml.cpac_yaml(pipeline1, pipeline2, f'{workspace}/correlations', run_name, 1)
     click.echo('YML obtained!')
     click.echo('** Starting Correlations **')
     
