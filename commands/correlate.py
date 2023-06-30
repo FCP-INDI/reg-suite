312bcc082322ab0a0d38d4d139ee50808e59768f
Theodore (Machine User) <CMI_CPAC_Support@childmind.org>
2023-06-30 12:23:39 -0400

    data will be downloaded from s3 through git actions, not repo

--- commands/correlate.py
+++ commands/correlate.py
@@ -2,32 +2,25 @@ import os
 import click
 import subprocess
 from commands.bin import parse_yaml
-from commands.bin import utils
-
-def download_output(workspace, git_home):
-
-    project = '39vqd'
-    datapath = utils.download_data(workspace, project, git_home)
-
-    return datapath
 
 
 @click.command()
-@click.option('--outdir', '-o', required=True, type=str, help='Path to output directory from regression run'
-            'done using the `run` command.')
+@click.option('--pipeline1', '-o', required=True, type=str, help='Path to output directory from CPAC run '
+              'to correlate against pipeline2')
+@click.option('--pipeline2', '-o', required=True, type=str, help='Path to output directory from CPAC run '
+              'to correlate against pipeline1')
 @click.option('--workspace','-w', type=str, help = 'Github workspace. This input is generated in git actions '
               'and should remain as $GITHUB_WORKSPACE')
 
 
-def correlate(outdir, workspace):
+def correlate(pipeline1, pipeline2, workspace):
     """
     Correlate outputs from regression run again another C-PAC version.
     """
 
     git_home = os.path.normpath(os.path.dirname(os.path.abspath(__file__)) + os.sep + os.pardir)
-    datapath = utils.download_data(workspace, git_home)
 
-    parse_yaml.cpac_yaml(datapath, outdir, f'{workspace}/correlations', 'run', 1)
+    parse_yaml.cpac_yaml(pipeline1, pipeline2, f'{workspace}/correlations', 'run', 1)
     click.echo('YML obtained!')
     click.echo('** Starting Correlations **')
     
